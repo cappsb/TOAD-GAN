14e448abb47560430d70f937c82c097720d2b5cb
cappsb <63438571+cappsb@users.noreply.github.com>
2021-03-20 21:52:08 -0500

    #2 created simplified tokens

--- megaman/tokens.py
+++ megaman/tokens.py
@@ -1,7 +1,6 @@
 from collections import OrderedDict
 
 # Dictionaries sorting Tokens by hierarchy. Hierarchies are based on game importance and similarity.
-
 NULL_TOKENS = OrderedDict(
     {
         "@": "null",
@@ -11,8 +10,6 @@ NULL_TOKENS = OrderedDict(
 SOLID_TOKENS = OrderedDict(
     {
         "#": "solid",
-        "C": "solid",
-        "A": "solid",
      }
 )
 
@@ -23,15 +20,6 @@ PASSABLE_TOKENS = OrderedDict(
         "B": "breakable",
         "M": "moving platform",
         "~": "water",
-        "L": "air",
-        "l": "air",
-        "W": "air",
-        "w": "air",
-        "+": "air",
-        "D": "air",
-        "U": "air",
-        "t": "air",
-        "*": "air",
     }
 )
 
@@ -40,35 +28,14 @@ PASSABLE_TOKENS = OrderedDict(
 
 ENEMY_TOKENS = OrderedDict(
     {    
-        "a": "enemy",
-        "b": "enemy",
-        "<": "enemy",
-        "^": "enemy",
-        "c": "enemy",
-        "d": "enemy",
         "e": "enemy",
-        "f": "enemy",
-        "g": "enemy",
-        "h": "enemy",
-        "i": "enemy",
-        "j": "enemy",
-        "k": "enemy",
-        "m": "enemy",
-        "n": "enemy",
-        "o": "enemy",
-        "p": "enemy",
-        "q": "enemy",
-        "r": "enemy",
-
     }
 )
 SPECIAL_TOKENS = OrderedDict(
     {
         "H": "hazard",
         "P": "player",
-        
         "Z": "level orb",
-        
      }
 )
 
@@ -89,3 +56,109 @@ TOKENS = OrderedDict(
 TOKEN_GROUPS = [NULL_TOKENS, SOLID_TOKENS, PASSABLE_TOKENS, ENEMY_TOKENS, SPECIAL_TOKENS]
 
 REPLACE_TOKENS = {}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##MORE COMPLICATED!!!!
+# NULL_TOKENS = OrderedDict(
+#     {
+#         "@": "null",
+#     }
+# )
+
+# SOLID_TOKENS = OrderedDict(
+#     {
+#         "#": "solid",
+#         "C": "solid",
+#         "A": "solid",
+#      }
+# )
+
+# PASSABLE_TOKENS = OrderedDict(
+#     {
+#         "-": "air",
+#         "|": "ladder",
+#         "B": "breakable",
+#         "M": "moving platform",
+#         "~": "water",
+#         "L": "air",
+#         "l": "air",
+#         "W": "air",
+#         "w": "air",
+#         "+": "air",
+#         "D": "air",
+#         "U": "air",
+#         "t": "air",
+#         "*": "air",
+#     }
+# )
+
+
+
+
+# ENEMY_TOKENS = OrderedDict(
+#     {    
+#         "a": "enemy",
+#         "b": "enemy",
+#         "<": "enemy",
+#         "^": "enemy",
+#         "c": "enemy",
+#         "d": "enemy",
+#         "e": "enemy",
+#         "f": "enemy",
+#         "g": "enemy",
+#         "h": "enemy",
+#         "i": "enemy",
+#         "j": "enemy",
+#         "k": "enemy",
+#         "m": "enemy",
+#         "n": "enemy",
+#         "o": "enemy",
+#         "p": "enemy",
+#         "q": "enemy",
+#         "r": "enemy",
+
+#     }
+# )
+# SPECIAL_TOKENS = OrderedDict(
+#     {
+#         "H": "hazard",
+#         "P": "player",
+        
+#         "Z": "level orb",
+        
+#      }
+# )
+
+
+
+# TOKEN_DOWNSAMPLING_HIERARCHY = [
+#     NULL_TOKENS,
+#     SOLID_TOKENS,
+#     PASSABLE_TOKENS,
+#     ENEMY_TOKENS,
+#     SPECIAL_TOKENS,
+# ]
+
+# TOKENS = OrderedDict(
+#     {**NULL_TOKENS, **SOLID_TOKENS, **PASSABLE_TOKENS, **ENEMY_TOKENS, **SPECIAL_TOKENS}
+# )
+
+# TOKEN_GROUPS = [NULL_TOKENS, SOLID_TOKENS, PASSABLE_TOKENS, ENEMY_TOKENS, SPECIAL_TOKENS]
+
+# REPLACE_TOKENS = {}
